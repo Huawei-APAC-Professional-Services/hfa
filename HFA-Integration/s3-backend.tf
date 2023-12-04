a09bca85ce2b66b17a9d9de0e215192bf1e9e011
mu <agoodmu@gmail.com>
2023-12-04 17:51:30 +0800

    add backend

--- HFA-Integration/s3-backend.tf
+++ HFA-Integration/s3-backend.tf
@@ -47,3 +47,20 @@ data "terraform_remote_state" "hfa_network" {
     skip_s3_checksum            = true
   }
 }
+
+data "terraform_remote_state" "hfa_app" {
+  backend = "s3"
+  config = {
+    bucket                      = "terraform-state-singapore"
+    key                         = "hfa-app/terraform.tfstate"
+    region                      = "ap-southeast-3"
+    endpoints = {
+      s3 = "https://obs.ap-southeast-3.myhuaweicloud.com"
+    }
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
+}
